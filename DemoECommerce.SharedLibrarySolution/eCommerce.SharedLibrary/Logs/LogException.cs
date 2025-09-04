25239245b2453aeda988850011838bbaa09df701
canerrsenol <canerrsenol@hotmail.com>
2025-09-04 18:15:09 +0300

    +SharedLibrary added

---
+++ DemoECommerce.SharedLibrarySolution/eCommerce.SharedLibrary/Logs/LogException.cs
@@ -0,0 +1,20 @@
+﻿using Serilog;
+
+namespace eCommerce.SharedLibrary.Logs
+{
+    public static class LogException
+    {
+        public static void LogExceptions(Exception ex)
+        {
+            LogToFile(ex.Message);
+            LogToConsole(ex.Message);
+            LogToDebug(ex.Message);
+        }
+
+        public static void LogToFile(string message) => Log.Information(message);
+
+        public static void LogToConsole(string message) => Log.Warning(message);
+
+        public static void LogToDebug(string message) => Log.Debug(message);
+    }
+}
---
+++ DemoECommerce.SharedLibrarySolution/eCommerce.SharedLibrary/Middleware/GlobalException.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eCommerce.SharedLibrary.Middleware
+{
+    public class GlobalException(RequestDelegate next)
+    {
+        public async Task InvokeAsync(HttpContext context)
+        {
+            // Declare variables
+            string message = "sorry, internal server error occured";
+            int statusCode = (int)HttpStatusCode.InternalServerError;
+            string title = "Error";
+
+            try
+            {
+                await next(context);
+
+                // Check if Exception is Too Many Requests // 429 status code.
+                if (context.Response.StatusCode == StatusCodes.Status429TooManyRequests)
+                {
+                    title = "Warning";
+                    message = "Too many requests. Please try again later.";
+                    statusCode = (int)context.Response.StatusCode;
+
+                    await ModifyHeader(context, title, message, statusCode);
+                }
+            }
+            catch (Exception ex)
+            {
+                // Log the exception (you can use a logging framework here)
+
+            }
+        }
+
+        private async Task ModifyHeader(HttpContext context, string title, string message, int statusCode)
+        {
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = statusCode;
+            var result = System.Text.Json.JsonSerializer.Serialize(new { title = title, message = message, statusCode = statusCode });
+            await context.Response.WriteAsync(result);
+        }
+    }
+}
