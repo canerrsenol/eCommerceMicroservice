dbaab3a75814d45790c8437b91d98e7ca4f63c35
canerrsenol <canerrsenol@hotmail.com>
2025-09-10 18:23:47 +0300

    +

--- DemoECommerce.SharedLibrarySolution/eCommerce.SharedLibrary/DependencyInjection/JWTAuthenticationSceme.cs
+++ DemoECommerce.SharedLibrarySolution/eCommerce.SharedLibrary/DependencyInjection/JWTAuthenticationSceme.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using System.Text;
 
 namespace eCommerce.SharedLibrary.DependencyInjection
 {
@@ -13,8 +14,25 @@ namespace eCommerce.SharedLibrary.DependencyInjection
             services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
                 .AddJwtBearer("Bearer", options =>
                 {
-                   
+                    var key = Encoding.UTF8.GetBytes(config.GetSection("Authentication:Key").Value);
+                    string issuer = config.GetSection("Authentication:Issuer").Value;
+                    string audience = config.GetSection("Authentication:Audience").Value;
+
+                    options.RequireHttpsMetadata = false;
+                    options.SaveToken = true;
+                    options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+                    {
+                        ValidateIssuer = true,
+                        ValidateAudience = true,
+                        ValidateLifetime = false,
+                        ValidateIssuerSigningKey = true,
+                        ValidIssuer = issuer,
+                        ValidAudience = audience,
+                        IssuerSigningKey = new Microsoft.IdentityModel.Tokens.SymmetricSecurityKey(key),
+                    };
                 }); 
+
+            return services;
         }
     }
 }
---
+++ DemoECommerce.SharedLibrarySolution/eCommerce.SharedLibrary/DependencyInjection/SharedServiceContainer.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace eCommerce.SharedLibrary.DependencyInjection
+{
+    public static class SharedServiceContainer
+    {
+        public static IServiceCollection AddSharedServices<TContext>(this IServiceCollection services
+            , IConfiguration config, string fileName) where TContext : DbContext
+        {
+            return services;
+        }
+    }
+}
