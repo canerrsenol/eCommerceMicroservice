fe55f5711663985f5d9bacdd8b49e7ca0d1bb601
canerrsenol <canerrsenol@hotmail.com>
2025-09-12 17:57:54 +0300

    +

---
+++ DemoECommerce.ProductApiSolution/ProductApi.Application/DTOs/Convertions/ProductConversions.cs
@@ -0,0 +1,28 @@
+﻿using ProductApi.Domain.Entities;
+
+namespace ProductApi.Application.DTOs.Convertions
+{
+    public static class ProductConversions
+    {
+        public static ProductDTO ToEntity(ProductDTO product) => new()
+        {
+            Id = product.Id,
+            Name = product.Name,
+            Quantity = product.Quantity,
+            Price = product.Price
+        };
+
+        public static (ProductDTO?, IEnumerable<ProductDTO>?) FromEntity(Product product, IEnumerable<Product>? products)
+        (
+            if(product is not null || products is null)
+            {
+
+
+
+            Id: product.Id,
+            Name: product.Name!,
+            Quantity: product.Quantity,
+            Price: product.Price
+        );
+    }
+}
--- DemoECommerce.SharedLibrarySolution/eCommerce.SharedLibrary/DependencyInjection/SharedServiceContainer.cs
+++ DemoECommerce.SharedLibrarySolution/eCommerce.SharedLibrary/DependencyInjection/SharedServiceContainer.cs
@@ -1,5 +1,9 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using eCommerce.SharedLibrary.Middleware;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Serilog;
 
 namespace eCommerce.SharedLibrary.DependencyInjection
 {
@@ -8,7 +12,38 @@ namespace eCommerce.SharedLibrary.DependencyInjection
         public static IServiceCollection AddSharedServices<TContext>(this IServiceCollection services
             , IConfiguration config, string fileName) where TContext : DbContext
         {
+            // Add Generic Database Context
+
+            services.AddDbContext<TContext>(option => option.UseSqlServer(
+                config.GetConnectionString("eCommerceConnection"), 
+                sqlserverOption => sqlserverOption.EnableRetryOnFailure()));
+
+            // Configure serilog logging
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Information()
+                .WriteTo.Debug()
+                .WriteTo.Console()
+                .WriteTo.File(path: $"{fileName}-.text",
+                restrictedToMinimumLevel: Serilog.Events.LogEventLevel.Information,
+                outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} [{Level:u3}] {message:lj}{NewLine}{Exception}",
+                rollingInterval: RollingInterval.Day)
+                .CreateLogger();
+
+            // Add JWT Authentication Scheme
+            JWTAuthenticationSceme.AddJWTAuthenticationSceme(services, config);
+
             return services;
+        }
+
+        public static IApplicationBuilder UseSharedPolicies(this IApplicationBuilder app)
+        {
+            // Use global Exception
+            app.UseMiddleware<GlobalException>();
+
+            // Register the middleware to block all outsiders API calls.
+            app.UseMiddleware<ListenToOnlyApiGateway>();
+
+            return app;
         }
     }
 }
