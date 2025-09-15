5a3accb221f12db2e15c4984a82370ba01f72050
canerrsenol <canerrsenol@hotmail.com>
2025-09-15 18:38:52 +0300

    +

--- DemoECommerce.ProductApiSolution/ProductApi.Application/DTOs/Convertions/ProductConversions.cs
+++ DemoECommerce.ProductApiSolution/ProductApi.Application/DTOs/Convertions/ProductConversions.cs
@@ -4,7 +4,7 @@ namespace ProductApi.Application.DTOs.Convertions
 {
     public static class ProductConversions
     {
-        public static ProductDTO ToEntity(ProductDTO product) => new()
+        public static Product ToEntity(ProductDTO product) => new()
         {
             Id = product.Id,
             Name = product.Name,
@@ -13,16 +13,39 @@ namespace ProductApi.Application.DTOs.Convertions
         };
 
         public static (ProductDTO?, IEnumerable<ProductDTO>?) FromEntity(Product product, IEnumerable<Product>? products)
-        (
-            if(product is not null || products is null)
+        {
+            // If a single product is provided (not null), return it
+            if (product is not null)
             {
+                var singleProduct = new ProductDTO
+                (
+                    product.Id,
+                    product.Name!,
+                    product.Quantity,
+                    product.Price
+                );
 
+                return (singleProduct, null);
+            }
 
+            // If a list of products is provided (not null), return the list
+            if (products is not null || product is not null)
+            {
+                var _products = products.Select(pDTO =>
+                    new ProductDTO
+                    (
+                        pDTO!.Id,
+                        pDTO.Name!,
+                        pDTO.Quantity,
+                        pDTO.Price
+                    )
+                ).ToList();
 
-            Id: product.Id,
-            Name: product.Name!,
-            Quantity: product.Quantity,
-            Price: product.Price
-        );
+                return (null, _products);
+            }
+
+            // If both are null, return nulls
+            return (null, null);
+        }
     }
 }
